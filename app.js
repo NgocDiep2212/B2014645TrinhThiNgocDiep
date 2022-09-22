e2135dd979328c6555f05d67dd1d9ec37bade6ce
NgocDiep2212 <duckcute44112002@gmail.com>
2022-09-22 16:15:31 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -12,5 +12,7 @@ app.get("/",(req,res)=>{
         message: "Welcome to contact book application."
     });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
